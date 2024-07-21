d7b8c2e7da9c1a57055d61771a6fbfe26f98c967
Owen Klan <owen.j.klan@gmail.com>
2024-07-21 16:40:42 +1000

    Allow auto-updates when code changes

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_api_gateway_resource" "resource" {
-  path_part   = "resource"
+  path_part   = "{filename}"
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -21,7 +21,7 @@ resource "aws_lambda_function" "download_file" {
     filename = "lambda_function_payload.zip"
     function_name = "download-file-blog"
     role = aws_iam_role.iam_for_lambda.arn
-
+    source_code_hash = data.archive_file.lambda.output_base64sha256
     handler = "download_file.lambda_handler"
     runtime = "python3.12"
 }
