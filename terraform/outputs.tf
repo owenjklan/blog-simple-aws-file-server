e9a513e01faabf31f593dd4c2ba495dd6f2bb64a
Owen Klan <owen.j.klan@gmail.com>
2024-07-22 20:53:29 +1000

    Put outputs back to state as discussed in article

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,3 @@
 output "endpoint_url" {
-  # invoke_url already ends with a slash
-  value = "${aws_api_gateway_deployment.deployment.invoke_url}${aws_api_gateway_stage.deploy_stage.stage_name}/"
+  value = aws_api_gateway_deployment.deployment.invoke_url
 }
